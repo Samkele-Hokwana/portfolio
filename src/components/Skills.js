b49422b38062de5acfa037c870699849f48d96b0
Oyintanda Samkele Hokwana <s217612792@Mandela.ac.za>
2026-01-15 04:18:00 +0200

    Edit: Skills heading animation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,11 +5,11 @@ import React from 'react'
 const Skills = () => {
   return (
     <div className='skills'>
-        <h1>Skills</h1>
+        <h1 className='hSkills'>Skills</h1>      
         <div>
           <p><strong>Languages:</strong> Java, C#, HTML, CSS, Javascript(React)</p>
           <p><strong>Databases:</strong> MySQL, Ms SQL server</p>
-          <p><strong>Frameworks & Libraries:</strong> Node js, Bootstrap, JavaFX, SpringBoot</p>
+          <p><strong>Frameworks & Libraries:</strong> Bootstrap, JavaFX, SpringBoot</p>
           <p><strong>Version Control:</strong> Git, GitHub</p>  
         </div>
       </div>
